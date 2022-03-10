2f7915a19b10028a70ee87b631c49a0dd23272ce
muhammadshujja <muhammadshujja123@gmail.com>
2022-03-10 19:50:52 +0500

    string

--- main.dart
+++ main.dart
@@ -1,25 +1,5 @@
 main() {
-  String name = "Muhammad Shujja";
-  int age = 27;
-  double height = 1.4;
-  String address = "House no#L-962 dhoke khabba quomi road Rwp";
-  print("My name is $name.My age is $age.My height is $height.");
-  print(address);
-  //string length
-  print(name.length); // 15
-  print(name.isEmpty); // false
-  print(name.isNotEmpty); // true
-  print(name.toUpperCase()); // MUHAMMAD SHUJJA
-  print(name.toLowerCase()); // muhammad shujja
-  print(name.trimLeft()); // clear white spaces from lest side
-  print(name.trimRight()); // clear white spaces from right side
-  print(name.trim()); // clear whire spaces from both sides
-  print(name.substring(8, 15)); // Shujja
-  print(name.replaceAll('Shujja', 'shuja')); // replace shujja to shuja
-  print(name.codeUnits);
-  if (name.length < 20) {
-    print("this string is less then and equal to 20");
-  } else {
-    print("this string is eqaul to 20");
-  }
+  String name = "My name is shujja";
+  var arr = name.split(" ");
+  print(arr[3]);
 }
